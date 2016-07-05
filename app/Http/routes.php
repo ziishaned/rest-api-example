918ce2a220ad20756f40f328cddf9630b784f72d
Zeeshan Ahmed <ziishaned@gmail.com>
2016-07-05 11:34:14 +0500

    Add prefix to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,23 +1,30 @@
 <?php
 
-$app->get('users', ['uses' => 'UsersController@getUsers', ]);
-$app->get('users/{id}/posts', ['uses' => 'UsersController@getUserPosts', ]);
-$app->get('users/{id}/posts/{postId}', ['uses' => 'UsersController@getUserPost', ]);
-$app->get('users/{id}/comments', ['uses' => 'UsersController@getUserComments', ]);
-$app->get('users/{id}/comments/{commentId}', ['uses' => 'UsersController@getUserComment', ]);
-$app->get('users/{id}', ['uses' => 'UsersController@getUser', ]);
-$app->post('users', ['uses' => 'UsersController@createUser', ]);
-$app->delete('users/{id}', ['uses' => 'UsersController@deleteUser', ]);
-$app->put('users/{id}', ['uses' => 'UsersController@updateUser', ]);
+$app->group(['prefix' => 'users', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
+	$app->get('/', ['uses' => 'UsersController@getUsers', ]);
+	$app->get('{id}/posts', ['uses' => 'UsersController@getUserPosts', ]);
+	$app->get('{id}/posts/{postId}', ['uses' => 'UsersController@getUserPost', ]);
+	$app->get('{id}/comments', ['uses' => 'UsersController@getUserComments', ]);
+	$app->get('{id}/comments/{commentId}', ['uses' => 'UsersController@getUserComment', ]);
+	$app->get('{id}', ['uses' => 'UsersController@getUser', ]);
+	$app->post('/', ['uses' => 'UsersController@createUser', ]);
+	$app->delete('{id}', ['uses' => 'UsersController@deleteUser', ]);
+	$app->put('{id}', ['uses' => 'UsersController@updateUser', ]);
+});
 
-$app->get('posts', ['uses' => 'PostsController@getPosts', ]);
-$app->get('posts/{id}', ['uses' => 'PostsController@getPost', ]);
-$app->post('posts', ['uses' => 'PostsController@createPost', ]);
-$app->delete('posts/{id}', ['uses' => 'PostsController@deletePost', ]);
-$app->put('posts/{id}', ['uses' => 'PostsController@updatePost', ]);
 
-$app->get('comments', ['uses' => 'CommentsController@getComments', ]);
-$app->get('comments/{id}', ['uses' => 'CommentsController@getComment', ]);
-$app->post('comments', ['uses' => 'CommentsController@createComment', ]);
-$app->delete('comments/{id}', ['uses' => 'CommentsController@deleteComment', ]);
-$app->put('comments/{id}', ['uses' => 'CommentsController@updateComment', ]);
+$app->group(['prefix' => 'posts', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
+	$app->get('/', ['uses' => 'PostsController@getPosts', ]);
+	$app->get('{id}', ['uses' => 'PostsController@getPost', ]);
+	$app->post('/', ['uses' => 'PostsController@createPost', ]);
+	$app->delete('{id}', ['uses' => 'PostsController@deletePost', ]);
+	$app->put('{id}', ['uses' => 'PostsController@updatePost', ]);
+});
+
+$app->group(['prefix' => 'comments', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
+	$app->get('/', ['uses' => 'CommentsController@getComments', ]);
+	$app->get('{id}', ['uses' => 'CommentsController@getComment', ]);
+	$app->post('/', ['uses' => 'CommentsController@createComment', ]);
+	$app->delete('{id}', ['uses' => 'CommentsController@deleteComment', ]);
+	$app->put('{id}', ['uses' => 'CommentsController@updateComment', ]);
+});
